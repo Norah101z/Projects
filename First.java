8ec53be21ac5cb54497f41b45327a43307b25950
Norah101z <anderson_norah@students.columbusstate.edu>
2026-01-26 12:26:42 -0500

    First Java

---
+++ First.java
@@ -0,0 +1,13 @@
+//Norah Anderson
+//First Java program for CS II
+
+public class First {
+  public static void main(String[] args) {
+      System.out.println("Hello Everyone!");
+      System.out.println("Job title: Information Security Analyst ");
+      System.out.println("Why: I enjoy solving security problems and protecting systems");
+      System.out.println("Median salary: $124,910");
+  }
+
+  }
+
